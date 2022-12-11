c4aa91794e012f50dcd53f8892a207a690fe02f8
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-11 15:07:47 +0530

    Added: calculate employee daily wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,12 @@
-echo "Welcome to Employee Wage Computation program"
+
+isPresent=1
+randomCheck=$((RANDOM%2));
+
+if [[ $isPresent -eq $randomCheck ]]
+then 
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
+else
+	salary=0;
+fi
