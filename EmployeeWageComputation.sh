f201d293681f49307135eb4be51aa5f5eb0ab20a
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-11 14:42:48 +0530

    Added:Display Employee Present or Absent

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,1 +1,11 @@
 echo "Welcome to Employee Wage Computation Program"
+
+isPresent=1
+randomCheck=$((RANDOM%2));
+
+if [[ $isPresent -eq $randomCheck ]]
+then 
+	echo "Employee is present" 
+else
+	echo "Employee is absent"
+fi
