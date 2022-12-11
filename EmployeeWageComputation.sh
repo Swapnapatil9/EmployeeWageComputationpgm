4956f18f8d8a44d6ffcce87d2fd8d1010a24cdef
“Swapnapatil9” <swapnapatil629@gmail.com>
2022-12-11 20:43:08 +0530

    Added: Store the Day and the Daily Wage along with the Total Wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,39 +1,41 @@
 #!/bin/bash -x
 
-function calculateWorkingHour() {
-	case $1 in
-		0)
-			workingHour=0;
-			;;
-
-		1)
-			workingHour=8;
-			;;
-		2)
-			workingHour=4;
-			;;
-	esac;
-	echo $workingHour;
-}
-
 perHourSalary=20;
+workingHour=0;
 totalSalary=0;
 totalWorkingHour=0;
 day=1;
-
 while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
 do
-	wHour=$(calculateWorkingHour $((RANDOM%3)));
-	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
+
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
 	if [ $totalWorkingHour -gt 40 ]
 	then
-		totalWorkingHour=$(($totalWorkingHour - $wHour));
+		totalWorkingHour=$(($totalWorkingHour - $workingHour));
 		break;
 	fi
-	salary=$(($perHourSalary * $wHour));
+	salary=$(($perHourSalary * $workingHour));
 	totalSalary=$(($totalSalary + $salary));
 	((day++));
+	echo "Daily wage:"${salary[@]}
+	echo "Total wage:"${totalSalary[@]}
+	echo "Days:"${day[@]}
 done
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
- 
 
